af6e2842af1df8c23d5206309bc89101b0bdd36f
Ghiță Bizău <ghita_bizau@yahoo.com>
2016-05-24 13:07:14 +0300

    [refs #71544] Use script for events in homepage.

--- src/eni/seis/content/browser/homepage.py
+++ src/eni/seis/content/browser/homepage.py
@@ -1,6 +1,4 @@
-from plone import api
 from Products.Five.browser import BrowserView
-from eni.seis.content.util import EventItem
 from eni.seis.content.util import portal_absolute_url
 from eni.seis.content.util import is_east_website
 from eni.seis.content.util import is_south_website
@@ -9,16 +7,6 @@ from eni.seis.content.util import is_south_website
 class HomepageView(BrowserView):
     """ Custom homepage
     """
-
-    def get_events(self):
-        catalog = api.portal.get_tool('portal_catalog')
-        query = {
-            'portal_type': 'Event',
-            'sort_on': 'start',
-            'sort_order': 'ascending'
-        }
-
-        return [EventItem(brain) for brain in catalog(**query)]
 
     def all_events_url(self):
         return portal_absolute_url() + "/events"
