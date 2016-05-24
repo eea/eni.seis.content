257f72ae35482b956f71e4df5e8edb7076635bcd
Ghiță Bizău <ghita_bizau@yahoo.com>
2016-05-24 17:46:02 +0300

    [refs #71544] Add views to check website type. Update docs.

--- src/eni/seis/content/browser/views.py
+++ src/eni/seis/content/browser/views.py
@@ -1,5 +1,7 @@
 from Products.Five.browser import BrowserView
 from eni.seis.content.util import portal_absolute_url
+from eni.seis.content.util import is_east_website
+from eni.seis.content.util import is_south_website
 
 
 class HomepageView(BrowserView):
@@ -12,3 +14,17 @@ class PortalAbsoluteUrlView(BrowserView):
     """
     def __call__(self):
         return portal_absolute_url()
+
+
+class IsEastWebsiteView(BrowserView):
+    """ Return True for EAST website else False
+    """
+    def __call__(self):
+        return is_east_website(self.request)
+
+
+class IsSouthWebsiteView(BrowserView):
+    """ Return True for SOUTH website else False
+    """
+    def __call__(self):
+        return is_south_website(self.request)
