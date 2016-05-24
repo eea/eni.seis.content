4f5d507d7459c89d0270c17ee3b692fad613ffb7
Ghiță Bizău <ghita_bizau@yahoo.com>
2016-05-24 14:34:42 +0300

    [refs #71544] More clean code.

--- src/eni/seis/content/browser/views.py
+++ src/eni/seis/content/browser/views.py
@@ -1,18 +1,10 @@
 from Products.Five.browser import BrowserView
 from eni.seis.content.util import portal_absolute_url
-from eni.seis.content.util import is_east_website
-from eni.seis.content.util import is_south_website
 
 
 class HomepageView(BrowserView):
     """ Custom homepage
     """
-
-    def is_east_website(self):
-        return is_east_website(self.request)
-
-    def is_south_website(self):
-        return is_south_website(self.request)
 
 
 class PortalAbsoluteUrlView(BrowserView):
