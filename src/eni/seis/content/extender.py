adcd4a618af196b24ad8c88960689c53134cf275
Ghiță Bizău <ghita_bizau@yahoo.com>
2016-11-16 13:05:56 +0200

    Extender, long_description field.

---
+++ src/eni/seis/content/extender.py
@@ -0,0 +1,40 @@
+from archetypes.schemaextender.interfaces import ISchemaExtender
+from Products.Archetypes.atapi import RichWidget
+from Products.Archetypes.atapi import TextField
+from Products.ATContentTypes.interfaces import IATFolder
+from zope.component import adapts
+from zope.interface import implements
+from archetypes.schemaextender.field import ExtensionField
+
+
+class ExtensionTextField(ExtensionField, TextField):
+    """ derivative of stringfield for extending schemas """
+
+
+class DescriptionFieldExtender(object):
+
+    adapts(IATFolder)
+    implements(ISchemaExtender)
+
+    _fields = [
+
+        ExtensionTextField(
+            'long_description',
+            required=False,
+            schemata='default',
+            searchable=True,
+            primary=True,
+            default_output_type='text/x-html-safe',
+            widget=RichWidget(
+                description='',
+                label='Body Text',
+                rows=25,
+            ),
+        ),
+    ]
+
+    def __init__(self, context):
+        self.context = context
+
+    def getFields(self):
+        return self._fields
