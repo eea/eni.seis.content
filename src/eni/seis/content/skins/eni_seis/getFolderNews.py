887340d5e566864263069fd249ace290c1755341
Andrei Meliș <andrei.melis@eaudeweb.ro>
2016-11-22 14:34:06 +0200

    Add countries on News; Tabular view for news.

---
+++ src/eni/seis/content/skins/eni_seis/getFolderNews.py
@@ -0,0 +1,17 @@
+## Script (Python) "getFolderContents"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=contentFilter=None,batch=False,b_size=100,full_objects=False
+##title=wrapper method around to use catalog to get folder contents
+
+if not contentFilter:
+    contentFilter = {
+        'portal_type': ['News Item'],
+        'sort_on': 'effective',
+        'sort_order': 'descending'
+    }
+
+return context.getFolderContents(contentFilter, batch, b_size, full_objects)
