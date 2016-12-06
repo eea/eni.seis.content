837f93e704ca9d8419358df86c5e5ec0c8b9bf8e
Tiberiu Ichim <tiberiu@pixelblaster.ro>
2016-12-06 10:44:32 +0200

    Added countries to meeting_index

--- src/eni/seis/content/skins/eni_seis/event_countries_vocab.py
+++ src/eni/seis/content/skins/eni_seis/event_countries_vocab.py
@@ -1,20 +1,39 @@
-return {
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=key_based=False
+##title=format dates for an event
 
-'Algeria': 'dz',
-'Egypt': 'eg',
-'Israel': 'il',
-'Jordan': 'jo',
-'Lebanon': 'lb',
-'Libya': 'ly',
-'Marocco': 'ma',
-'Palestine': 'ps',
-'Tunisia': 'tn',
+pairs = [
+    ('Algeria', 'dz'),
+    ('Egypt', 'eg'),
+    ('Israel', 'il'),
+    ('Jordan', 'jo'),
+    ('Lebanon', 'lb'),
+    ('Libya', 'ly'),
+    ('Morocco', 'ma'),
+    ('Palestine', 'ps'),
+    ('Tunisia', 'tn'),
 
-'Armenia': 'am',
-'Azerbaijan': 'az',
-'by': 'Belarus',
-'ge': 'Georgia',
-'md': 'Moldova',
-'ua': 'Ukraine',
+    ('Armenia', 'am'),
+    ('Azerbaijan', 'az'),
+    ('Belarus', 'by'),
+    ('Georgia', 'ge'),
+    ('Moldova', 'md'),
+    ('Ukraine', 'ua'),
+]
 
-}
+if key_based:
+    res = {}
+    for p in pairs:
+        res[p[1]] = p[0]
+    return res
+
+res = {}
+for p in pairs:
+    res[p[0]] = p[1]
+
+return res
+
