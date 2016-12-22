f7a360ca792b5736d116e23bccb0fdd04da8b704
mihai-macaneata <themacaneata@gmail.com>
2016-12-22 17:02:30 +0200

    added button menu for meetings

--- src/eni/seis/content/skins/eni_seis/event_countries_vocab.py
+++ src/eni/seis/content/skins/eni_seis/event_countries_vocab.py
@@ -1,5 +1,13 @@
+## Script (Python) "event_countries_vocab"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=key_based=False
+##title=format dates for an event
+##
 pairs = [
-
     ('Algeria', 'dz'),
     ('Egypt', 'eg'),
     ('Israel', 'il'),
@@ -16,8 +24,8 @@ pairs = [
     ('Georgia', 'ge'),
     ('Moldova', 'md'),
     ('Ukraine', 'ua'),
-
 ]
+
 if key_based:
     res = {}
     for p in pairs:
