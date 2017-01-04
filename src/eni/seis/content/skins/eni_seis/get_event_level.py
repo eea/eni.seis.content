5c551ce12fab845ba107b0a28bbb329fed38b3ae
Ghiță Bizău <ghita_bizau@yahoo.com>
2017-01-04 11:30:39 +0200

    Show event level on event view.

--- src/eni/seis/content/skins/eni_seis/get_event_level.py
+++ src/eni/seis/content/skins/eni_seis/get_event_level.py
@@ -3,20 +3,32 @@
 ##bind namespace=
 ##bind script=script
 ##bind subpath=traverse_subpath
-##parameters=brain
+##parameters=brain=None,event=None
 ##title=Get event level
 
-if brain.Type != 'Event':
-    return ''
 
 levels = {
     'Regional Level': 'regional',
     'National Level': 'national',
 }
 
-obj = brain.getObject()
-try:
-    event_level = obj.event_level
-except:
-    event_level = ''
-return levels.get(event_level, '')
+if brain is not None:
+    if brain.Type != 'Event':
+        return ''
+
+    obj = brain.getObject()
+
+    try:
+        event_level = obj.event_level
+    except:
+        event_level = ''
+    return levels.get(event_level, '')
+
+else:
+    if event is not None:
+        if event.portal_type == 'Event':
+            return event.event_level
+        else:
+            return ''
+
+return ""
