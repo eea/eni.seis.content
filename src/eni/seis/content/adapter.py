04ae6c59aefa8b8a71e35eef99fb33e432b20284
Ghiță Bizău <ghita_bizau@yahoo.com>
2017-04-27 18:05:12 +0300

    [refs #83535] Fixed fields for empty values on adding user.

--- src/eni/seis/content/adapter.py
+++ src/eni/seis/content/adapter.py
@@ -5,53 +5,79 @@ class EnhancedUserDataPanelAdapter(UserDataPanelAdapter):
     """
     """
     def get_first_name(self):
-        first_name = self.context.getProperty('first_name', '').decode('utf-8')
-        return first_name
+        value = self.context.getProperty('first_name', '')
+        if value is not None:
+            value = value.decode('utf-8')
+        return value
 
     def set_first_name(self, value):
-        first_name = value
+        if value is not None:
+            value = value.encode('utf-8')
         return self.context.setMemberProperties(
-            {'first_name': first_name.encode('utf-8')})
+            {'first_name': value})
     first_name = property(get_first_name, set_first_name)
 
     def get_last_name(self):
-        last_name = self.context.getProperty('last_name', '').decode('utf-8')
-        return last_name
+        value = self.context.getProperty('last_name', '')
+        if value is not None:
+            value = value.decode('utf-8')
+        return value
 
     def set_last_name(self, value):
-        last_name = value
+        if value is not None:
+            value = value.encode('utf-8')
         return self.context.setMemberProperties(
-            {'last_name': last_name.encode('utf-8')})
+            {'last_name': value})
     last_name = property(get_last_name, set_last_name)
 
     def get_telephone(self):
-        return self.context.getProperty('telephone', '').decode('utf-8')
+        value = self.context.getProperty('last_name', '')
+        if value is not None:
+            value = value.decode('utf-8')
+        return value
 
     def set_telephone(self, value):
+        if value is not None:
+            value = value.encode('utf-8')
         return self.context.setMemberProperties(
-            {'telephone': value.encode('utf-8')})
+            {'telephone': value})
     telephone = property(get_telephone, set_telephone)
 
     def get_institution(self):
-        return self.context.getProperty('institution', '').decode('utf-8')
+        value = self.context.getProperty('institution', '')
+        if value is not None:
+            value = value.decode('utf-8')
+        return value
 
     def set_institution(self, value):
+        if value is not None:
+            value = value.encode('utf-8')
         return self.context.setMemberProperties(
-            {'institution': value.encode('utf-8')})
+            {'institution': value})
     institution = property(get_institution, set_institution)
 
     def get_from_country(self):
-        return self.context.getProperty('from_country', '').decode('utf-8')
+        value = self.context.getProperty('from_country', '')
+        if value is not None:
+            value = value.decode('utf-8')
+        return value
 
     def set_from_country(self, value):
+        if value is not None:
+            value = value.encode('utf-8')
         return self.context.setMemberProperties(
-            {'from_country': value.encode('utf-8')})
+            {'from_country': value})
     from_country = property(get_from_country, set_from_country)
 
     def get_from_city(self):
-        return self.context.getProperty('from_city', '').decode('utf-8')
+        value = self.context.getProperty('from_city', '')
+        if value is not None:
+            value = value.decode('utf-8')
+        return value
 
     def set_from_city(self, value):
+        if value is not None:
+            value = value.encode('utf-8')
         return self.context.setMemberProperties(
-            {'from_city': value.encode('utf-8')})
+            {'from_city': value})
     from_city = property(get_from_city, set_from_city)
