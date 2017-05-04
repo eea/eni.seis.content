b3002c0c734202d234eb177a57710013ebcb5f88
David Batranu <david.batranu@eaudeweb.ro>
2017-05-04 15:47:23 +0300

    [refs #83535] User creation form.

--- src/eni/seis/content/browser/overrides/meeting.py
+++ src/eni/seis/content/browser/overrides/meeting.py
@@ -4,6 +4,18 @@ import plone.api as api
 from eea.meeting.browser import views
 
 
+FIELDS_REGISTRATION = (
+    ('first_name', 'first_name'),
+    ('last_name', 'last_name'),
+    ('position', 'position'),
+    ('organisation', 'institution'),
+    ('address', 'address'),
+    ('country', 'from_country'),
+    ('email', 'email'),
+    ('phone_numbers', 'phone_numbers'),
+)
+
+
 class Register(views.Register):
     index = ViewPageTemplateFile('meeting_register.pt')
 
@@ -37,3 +49,31 @@ class Register(views.Register):
         api.portal.show_message(
             'Invalid credentials.', request=self.request, type='error')
         return self.index()
+
+    def register(self):
+        pw1, pw2 = self.request.get('pw1'), self.request.get('pw2')
+        if pw1 != pw2:
+            return self.index()
+
+        member_data = {
+            member_key: self.request.get(form_key)
+            for form_key, member_key in FIELDS_REGISTRATION
+        }
+
+        member_data['fullname'] = '{} {}'.format(
+            member_data['first_name'],
+            member_data['last_name'],
+        )
+
+        member = api.user.create(
+            email=self.request.get('email'),
+            username=self.request.get('username'),
+            password=pw1,
+            properties=member_data,
+        )
+
+        acl = api.portal.get().acl_users
+        acl.session._setupSession(member.getId(), self.request.response)
+        return self.request.response.redirect(
+            self.context.absolute_url() + '/register'
+        )
