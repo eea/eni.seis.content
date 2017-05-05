a867d3a6da8c10947ad45d267b0e6bc26552b714
David Batranu <david.batranu@eaudeweb.ro>
2017-05-05 16:48:24 +0300

    [refs #83535] Cleaned up validation and messages.

--- src/eni/seis/content/browser/overrides/meeting.py
+++ src/eni/seis/content/browser/overrides/meeting.py
@@ -1,3 +1,4 @@
+from functools import partial
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 import plone.api as api
 
@@ -16,28 +17,104 @@ FIELDS_REGISTRATION = (
 )
 
 FIELDS_REQUIRED = (
-    'first_name',
-    'last_name',
-    'position',
-    'organisation',
-    'email',
+    ('first_name', 'First name'),
+    ('last_name', 'Last name'),
+    ('position', 'Position'),
+    ('organisation', 'Organisation'),
+    ('username', 'Username'),
+    ('email', 'Email'),
+    ('pw1', 'Password'),
 )
+
+
+def create_user(request):
+    member_data = {
+        member_key: request.get(form_key)
+        for form_key, member_key in FIELDS_REGISTRATION
+    }
+
+    member_data['fullname'] = '{} {}'.format(
+        member_data['first_name'],
+        member_data['last_name'],
+    )
+
+    return api.user.create(
+        email=request.get('email'),
+        username=request.get('username'),
+        password=request.get('pw1'),
+        properties=member_data,
+    )
+
+
+def login_user(response, user):
+    acl = api.portal.get().acl_users
+    acl.session._setupSession(user.getId(), response)
+
+
+def check_pw(request):
+    pw1, pw2 = request.get('pw1'), request.get('pw2')
+    return pw1 == pw2 or 'Passwords do not match!'
+
+
+def check_required(request):
+    missing_fields = [
+        label for fname, label in FIELDS_REQUIRED
+        if not request.get(fname)
+    ]
+
+    if missing_fields:
+        return (
+            'These fields are required: {}!'
+            .format(', '.join(missing_fields))
+        )
+
+
+REGISTER_VALIDATORS = (
+    check_pw,
+    check_required,
+)
+
+
+def register_error(request, template, msg):
+    return template(
+        register_message={'type': 'error', 'text': msg},
+        fields={
+            form_key: request.get(form_key)
+            for form_key, _ in FIELDS_REGISTRATION
+        }
+    )
 
 
 class Register(views.Register):
     index = ViewPageTemplateFile('meeting_register.pt')
 
     def __call__(self):
+        self.is_anon = api.user.is_anonymous()
+        self.response = self.request.response
+
         if self.request.method == 'POST':
             if 'submit.login' in self.request:
                 return self.login()
             elif 'submit.register' in self.request:
                 return self.register()
 
-        elif api.user.is_anonymous():
-            return self.index()
+        if not self.is_anon:
+            if self.request.get('login'):
+                return self.index(
+                    login_message={
+                        'type': 'good',
+                        'text': 'Login successful!'
+                    }
+                )
+            elif self.request.get('created'):
+                return self.index(
+                    created_message={
+                        'type': 'good',
+                        'text': 'User account created!',
+                    }
+                )
 
-        return super(Register, self).__call__()
+        return self.index()
 
     def login(self):
         name = self.request.get('__ac_name')
@@ -47,65 +124,31 @@ class Register(views.Register):
         user = acl.authenticate(name, pwd, self.request)
 
         if user is not None:
-
-            acl.session._setupSession(user.getId(), self.request.response)
-            return self.index(
-                login_message={'type': 'success', 'text': 'Login success!'}
-            )
+            login_user(self.response, user)
+            return self.response.redirect(
+                self.context.absolute_url() + '/register?login=true')
 
         return self.index(
             login_message={'type': 'error', 'text': 'Invalid credentials!'}
         )
 
     def register(self):
-        pw1, pw2 = self.request.get('pw1'), self.request.get('pw2')
+        err_msg = partial(register_error, self.request, self.index)
 
-        if pw1 != pw2:
-            return self.index(
-                register_message={
-                    'type': 'error',
-                    'text': 'Passwords do not match!'
-                },
-                fields={
-                    form_key: self.request.get(form_key)
-                    for form_key, _ in FIELDS_REGISTRATION
-                }
-            )
+        def reducer(acc, cur):
+            return '\n'.join((acc, cur)) if type(cur) == str else acc
 
-        missing_fields = [
-            fname for fname in FIELDS_REQUIRED if
-            fname not in self.request
-        ]
+        errors = reduce(reducer, (
+            name(self.request)
+            for name in REGISTER_VALIDATORS
+        ) , '')
+        if errors:
+            return err_msg(errors)
 
-        if missing_fields:
-            return self.index(
-                register_message={
-                    'type': 'error',
-                    'text': 'These fields are required: {}'.format(
-                        ', '.join(missing_fields)
-                    )
-                }
-            )
+        try:
+            login_user(self.response, create_user(self.request))
+        except Exception as e:
+            return err_msg(e.message)
 
-        member_data = {
-            member_key: self.request.get(form_key)
-            for form_key, member_key in FIELDS_REGISTRATION
-        }
-
-        member_data['fullname'] = '{} {}'.format(
-            member_data['first_name'],
-            member_data['last_name'],
-        )
-
-        member = api.user.create(
-            email=self.request.get('email'),
-            username=self.request.get('username'),
-            password=pw1,
-            properties=member_data,
-        )
-
-        acl = api.portal.get().acl_users
-        acl.session._setupSession(member.getId(), self.request.response)
-        return self.request.response.redirect(
-            self.context.absolute_url() + '/register'
-        )
+        return self.response.redirect(
+            self.context.absolute_url() + '/register?created=true')
