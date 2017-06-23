c3acb655d387d74f82a9088119b44c1bbc5ceacf
Ghiță Bizău <ghita_bizau@yahoo.com>
2017-06-23 16:52:38 +0300

    [refs #84441] Fix getLocalEvents to work for sub-sections of a country page.

--- src/eni/seis/content/skins/eni_seis/getLocalEvents.py
+++ src/eni/seis/content/skins/eni_seis/getLocalEvents.py
@@ -8,9 +8,18 @@ events = [
             sort_order='reverse')
         ]
 
-events = [ev
-    for ev in events
-    if vocab[context.Title()] in (ev.countries or [])
-]
+here_is_country = context.Title() in vocab
+parents = context.aq_inner.aq_parent.absolute_url().split('/')[3:]
+parents_paths = [
+    '/' + '/'.join(parents[0:x+1]) + '/' for x in range(0, len(parents))]
+find_parent_country_title = [
+    context.restrictedTraverse(x).Title() for x in parents_paths if
+    context.restrictedTraverse(x).Title() in vocab]
+a_parent_country_title = find_parent_country_title[0] if len(
+    find_parent_country_title) > 0 else None
+country_title = context.Title() if here_is_country else a_parent_country_title
+events = [
+    ev for ev in events if vocab[country_title] in (ev.countries or [])] if \
+    country_title is not None else events
 
 return events
