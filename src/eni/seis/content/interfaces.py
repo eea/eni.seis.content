eaa246d66c73adffd2b5013bd0803f459b0d00c3
Ghiță Bizău <ghita_bizau@yahoo.com>
2017-07-23 13:34:17 +0300

    [refs #86208] Report - using schema interface.

--- src/eni/seis/content/interfaces.py
+++ src/eni/seis/content/interfaces.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 """Module where all interfaces, events and exceptions live."""
 
+from plone.namedfile.field import NamedBlobFile
+from plone.supermodel import model
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
+from zope import schema
 
 
 class IEniSeisContentLayer(IDefaultBrowserLayer):
@@ -18,3 +21,19 @@ class IEniSeisSouthContentLayer(IDefaultBrowserLayer):
     """Marker interface that defines a browser layer.
        Used for SOUTH website.
     """
+
+
+class IReport(model.Schema):
+    """ Report item to be added in a country section
+    """
+    external_link = schema.URI(
+        title=u"External link",
+        required=False,
+        description=u"Report source if it is an external link."
+    )
+
+    file = NamedBlobFile(
+        title=u"File",
+        required=False,
+        description=u"Report file if it has one."
+    )
