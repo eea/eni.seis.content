5480d2e6dbd5a42a8e63bbc30714bd74bde6347b
Ghiță Bizău <ghita_bizau@yahoo.com>
2017-07-23 17:28:53 +0300

    [refs #86208] Report - category field - ordered options.

--- src/eni/seis/content/config.py
+++ src/eni/seis/content/config.py
@@ -1,23 +1,24 @@
 """Common configuration constants
 """
+from collections import OrderedDict
 from zope.i18nmessageid import MessageFactory as MF
 from zope.schema.vocabulary import SimpleVocabulary
 from zope.schema.vocabulary import SimpleTerm
 
 MessageFactory = MF('eni.seis')
 
-ALL_REPORTS_CATEGORIES = {
-    "A": "Air pollution and ozone depletion",
-    "B": "Climate change",
-    "C": "Water",
-    "D": "Biodiversity",
-    "E": "Land and soil",
-    "F": "Agriculture",
-    "G": "Energy",
-    "H": "Transport",
-    "I": "Waste",
-    "J": "Environmental financing"
-}
+ALL_REPORTS_CATEGORIES = OrderedDict([
+    ("A", "Air pollution and ozone depletion"),
+    ("B", "Climate change"),
+    ("C", "Water"),
+    ("D", "Biodiversity"),
+    ("E", "Land and soil"),
+    ("F", "Agriculture"),
+    ("G", "Energy"),
+    ("H", "Transport"),
+    ("I", "Waste"),
+    ("J", "Environmental financing")
+])
 
 REPORT_CATEGORIES_VOCAB = SimpleVocabulary(
     [
