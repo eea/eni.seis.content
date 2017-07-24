30679899df140374de1a51f1c62077de47b5dfe6
Ghiță Bizău <ghita_bizau@yahoo.com>
2017-07-24 14:52:09 +0300

    [refs #86208] WIP Countries view East - browser view.

--- src/eni/seis/content/browser/views.py
+++ src/eni/seis/content/browser/views.py
@@ -15,6 +15,16 @@ class HomepageView(BrowserView):
     """
 
 
+class CountriesViewEast(BrowserView):
+    """ The view for Countries section (East)
+    """
+
+    def test(self):
+        """ TODO Remove
+        """
+        return "ZZZ"
+
+
 class CountryViewEast(BrowserView):
     """ The view for a country (East)
     """
