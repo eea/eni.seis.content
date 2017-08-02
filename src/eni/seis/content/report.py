aeec80bf2fe48878667ef5cd5fb4dc4133fb88eb
Ghiță Bizău <ghita_bizau@yahoo.com>
2017-08-02 10:32:52 +0300

    [Refs #86208] WIP Reports - human readable report status.

--- src/eni/seis/content/report.py
+++ src/eni/seis/content/report.py
@@ -1,6 +1,7 @@
 from zope.interface import implementer
 from plone.dexterity.content import Container
 from eni.seis.content.interfaces import IReport
+from eni.seis.content.config import REPORTS_STATUS_TYPES
 
 
 REPORT_META_TYPE = 'Report'
@@ -30,3 +31,14 @@ class Report(Container):
         """ Return True if it has a link or a file, else False
         """
         return self.has_external_link() or self.has_file()
+
+    def get_status(self):
+        """ Return the status of the report
+            Example: Yes / Annual
+        """
+        status = ""
+        if self.status is not None:
+            status = REPORTS_STATUS_TYPES[self.status]
+            if len(self.status_details) > 0:
+                status = status + " / " + self.status_details
+        return status
