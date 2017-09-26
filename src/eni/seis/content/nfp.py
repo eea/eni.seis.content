c66b6c51ca0c977fb53ef119ab6d26395c67579c
Ghiță Bizău <ghita_bizau@yahoo.com>
2017-09-26 14:33:50 +0300

    [refs #88495] nfp: remove custom meta_type.

--- src/eni/seis/content/nfp.py
+++ src/eni/seis/content/nfp.py
@@ -3,11 +3,6 @@ from plone.dexterity.content import Container
 from eni.seis.content.interfaces import INationalFocalPoint
 
 
-NFP_META_TYPE = 'nfp'
-
-
 @implementer(INationalFocalPoint)
 class NationalFocalPoint(Container):
     """ nfp content type """
-
-    meta_type = NFP_META_TYPE
