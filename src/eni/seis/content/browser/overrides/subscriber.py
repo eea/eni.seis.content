00c146eaf2a324596f2c22bfacc9af9269de486e
Ghiță Bizău <ghita_bizau@yahoo.com>
2017-10-06 15:22:14 +0300

    [refs #88609] WIP custom edit form for meeting subscriber - add new fields.

--- src/eni/seis/content/browser/overrides/subscriber.py
+++ src/eni/seis/content/browser/overrides/subscriber.py
@@ -1,6 +1,109 @@
 from plone.dexterity.browser import edit
+from plone.dexterity.interfaces import IDexterityEditForm
+from plone.z3cform import layout
+from zope.interface import classImplements
+from plone.z3cform.fieldsets.extensible import FormExtender
+from z3c.form.field import Fields
+from zope import schema
+from eni.seis.content.config import MessageFactory as _
+
+
+class EditFormExtender(FormExtender):
+    def update(self):
+        if self.request.REQUEST_METHOD == 'GET':
+            # add fields
+            first_name = schema.TextLine(
+                __name__="first_name",
+                title=_(u'label_first_name', default=u'Name'),
+                description=_(u'help_first_name',
+                              default=u'Enter your first name.'),
+                required=True,
+            )
+
+            last_name = schema.TextLine(
+                __name__="last_name",
+                title=_(u'label_last_name', default=u'Surname'),
+                description=_(u'help_last_name',
+                              default=u'Enter your last name (family name).'),
+                required=True,
+            )
+
+            telephone = schema.TextLine(
+                __name__="telephone",
+                title=_(u'label_telephone', default=u'Phone number'),
+                description=_(u'help_telephone',
+                              default=u'Fill in the phone number'),
+                required=False
+            )
+
+            phone_numbers = schema.List(
+                __name__="phone_numbers",
+                title=_(u'label_phone_numbers', default=u'Phone numbers'),
+                description=_(u'help_phone_numbers',
+                              default=u'Fill in phone numbers.'),
+                value_type=schema.TextLine(),
+                required=False
+            )
+
+            institution = schema.TextLine(
+                __name__="institution",
+                title=_(u'label_institution', default=u'Institution'),
+                description=_(u'help_institution',
+                              default=u'Fill in the institution'),
+                required=True,
+            )
+
+            position = schema.TextLine(
+                __name__="position",
+                title=_(u'label_position', default=u'Position'),
+                description=_(u'help_position',
+                              default=u'Fill in the position'),
+                required=False,
+            )
+
+            from_country = schema.TextLine(
+                __name__="from_country",
+                title=_(u'label_from_country', default=u'From country'),
+                description=_(u'help_from_country',
+                              default=u'Fill in the From country'),
+                required=True,
+            )
+
+            from_city = schema.TextLine(
+                __name__="from_city",
+                title=_(u'label_from_city', default=u'From city'),
+                description=_(u'help_from_city',
+                              default=u'Fill in the From city'),
+                required=False,
+            )
+
+            address = schema.Text(
+                __name__="address",
+                title=_(u'label_address', default=u'Address'),
+                description=_(u'help_address',
+                              default=u'Fill in the address'),
+                required=True,
+            )
+
+            self.form.fields += Fields(first_name)
+            self.form.fields += Fields(last_name)
+            self.form.fields += Fields(telephone)
+            self.form.fields += Fields(phone_numbers)
+            self.form.fields += Fields(institution)
+            self.form.fields += Fields(position)
+            self.form.fields += Fields(from_country)
+            self.form.fields += Fields(from_city)
+            self.form.fields += Fields(address)
+
+        if self.request.REQUEST_METHOD == 'POST':
+            # save values
+            pass
 
 
 class EditForm(edit.DefaultEditForm):
     """ Override meeting subscriber edit form to add fields
     """
+
+
+EditView = layout.wrap_form(EditForm)
+classImplements(EditView, IDexterityEditForm)
