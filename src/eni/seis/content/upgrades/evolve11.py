aa1bf6cd10b14afb97fd82f3c85d23d5526376f8
Ghiță Bizău <ghita_bizau@yahoo.com>
2018-03-28 11:40:44 +0300

    [refs #94287] Add upgrade step: delete deprecated reports.

---
+++ src/eni/seis/content/upgrades/evolve11.py
@@ -0,0 +1,14 @@
+import logging
+from plone import api
+
+
+logger = logging.getLogger('eni.seis.content')
+
+
+def run(_):
+    catalog = api.portal.get_tool(name='portal_catalog')
+    reports = [b.getObject() for b in catalog(portal_type='report')]
+    deprecated_reports = [
+        x for x in reports if x.id == 'subnational-environmental-reports']
+
+    api.content.delete(objects=deprecated_reports)
