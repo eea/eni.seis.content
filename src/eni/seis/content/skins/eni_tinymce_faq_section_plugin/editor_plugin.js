ba1082b92cb7b1cca5a8ce9e216462996dd68b3b
Ghiță Bizău <ghita_bizau@yahoo.com>
2018-05-10 13:27:26 +0300

    [refs #92274] Fix file structure for plugin. Add toolbar settings.

--- src/eni/seis/content/skins/eni_tinymce_faq_section_plugin/editor_plugin.js
+++
@@ -1,5 +0,0 @@
-// Imported by upgrade step in: /portal_tinymce/@@tinymce-controlpanel - Resource Types - Custom Plugins:
-// eni_faq_section|portal_skins/eni_tinymce_faq_section_plugin/editor_plugin.js
-$(document).ready(function(){
-  alert("ENI FAQ Section WIP");
-});
