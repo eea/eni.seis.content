9c276736474ab961796d3b42fc8251ced5db32ff
Ghiță Bizău <ghita_bizau@yahoo.com>
2018-05-10 11:40:29 +0300

    [refs #92274] Upgrade step import tinymce settings and skins.

---
+++ src/eni/seis/content/skins/eni_tinymce_faq_section_plugin/editor_plugin.js
@@ -0,0 +1,5 @@
+// Imported by upgrade step in: /portal_tinymce/@@tinymce-controlpanel - Resource Types - Custom Plugins:
+// eni_faq_section|portal_skins/eni_tinymce_faq_section_plugin/editor_plugin.js
+$(document).ready(function(){
+  alert("ENI FAQ Section WIP");
+});
--- src/eni/seis/content/skins/eni_tinymce_override/custom_plugins/eni_faq_section/editor_plugin.js
+++
@@ -1,5 +0,0 @@
-// Add this in: /portal_tinymce/@@tinymce-controlpanel - Resource Types - Custom Plugins:
-// eni_faq_section|portal_skins/eni_tinymce_override/custom_plugins/eni_faq_section/editor_plugin.js
-$(document).ready(function(){
-  alert("ENI FAQ Section WIP");
-});
