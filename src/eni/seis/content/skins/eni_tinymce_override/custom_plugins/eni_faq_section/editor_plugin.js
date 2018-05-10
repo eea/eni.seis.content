b94a7e9103e8403e7ee23204c6de7149d9da8888
Ghiță Bizău <ghita_bizau@yahoo.com>
2018-05-10 10:25:21 +0300

    [refs #92274] Add plugin placeholder in skins.

---
+++ src/eni/seis/content/skins/eni_tinymce_override/custom_plugins/eni_faq_section/editor_plugin.js
@@ -0,0 +1,5 @@
+// Add this in: /portal_tinymce/@@tinymce-controlpanel - Resource Types - Custom Plugins:
+// eni_faq_section|portal_skins/eni_tinymce_override/custom_plugins/eni_faq_section/editor_plugin.js
+$(document).ready(function(){
+  alert("ENI FAQ Section WIP");
+});
