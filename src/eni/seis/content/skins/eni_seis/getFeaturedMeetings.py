97fe4c3f828928f9f0bd50414e97c1e9096f5fe4
Ghiță Bizău <ghita_bizau@yahoo.com>
2020-02-19 16:13:47 +0200

    [refs #114456] WIP script for featured meetings.

--- src/eni/seis/content/skins/eni_seis/getFeaturedMeetings.py
+++ src/eni/seis/content/skins/eni_seis/getFeaturedMeetings.py
@@ -1,10 +1,10 @@
 res = context.portal_catalog.searchResults(
-        portal_type=['News Item', 'Folderish News Item'],
+        portal_type=['eea.meeting'],
         review_state='published',
-        sort_on='effective',
+        sort_on='sort',
         sort_order='descending',
-        path='/south/communication/news'
+        path='/east/areas-of-work/communication/events'
         )
-news = [b.getObject() for b in res]
+meetings = [b.getObject() for b in res]
 
-return news
+return meetings
