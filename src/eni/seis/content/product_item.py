53f679701b8e427b055399a5c19ed7ddfba51d87
Ghiță Bizău <ghita_bizau@yahoo.com>
2020-03-16 20:03:33 +0200

    [refs #114456] New content type: ProductItem.

---
+++ src/eni/seis/content/product_item.py
@@ -0,0 +1,31 @@
+from zope.interface import implementer
+from plone.dexterity.content import Container
+from eni.seis.content.interfaces import IProductItem
+
+
+@implementer(IProductItem)
+class ProductItem(Container):
+    """ ProductItem content type """
+
+    def get_view_url(self):
+        """ Return its external link if exists or absolute_url
+            (because in some cases a report can act like a page with links or
+            some other content)
+        """
+        if self.has_external_link():
+            return self.external_link
+        return self.absolute_url()
+
+    def has_external_link(self):
+        """ Return True if it has a link, else False
+        """
+        if self.external_link:
+            return True
+        return False
+
+    def has_file(self):
+        """ Return True if it has a file, else False
+        """
+        if self.file:
+            return True
+        return False
