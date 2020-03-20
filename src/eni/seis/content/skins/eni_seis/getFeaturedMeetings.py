11f26884c4797f5f2a57590f97645916d396f7e7
Ghiță Bizău <ghita_bizau@yahoo.com>
2020-03-20 13:55:38 +0200

    [refs #114456] Fix meetings order.

--- src/eni/seis/content/skins/eni_seis/getFeaturedMeetings.py
+++ src/eni/seis/content/skins/eni_seis/getFeaturedMeetings.py
@@ -1,19 +1,11 @@
 res = context.portal_catalog.searchResults(
         portal_type=['eea.meeting'],
         review_state='published',
-        sort_on='sort',
-        sort_order='descending',
+        sort_on='start',
+        sort_order='ascending',
         path='/east/areas-of-work/communication/events'
         )
 meetings = [b for b in res]
 featured = [x for x in meetings if x.getObject().is_featured is True]
 
-regional = []
-national = []
-for x in featured:
-    if context.get_event_level(x) == 'regional':
-        regional.append(x)
-    elif context.get_event_level(x) == 'national':
-        national.append(x)
-
-return regional + national
+return featured
