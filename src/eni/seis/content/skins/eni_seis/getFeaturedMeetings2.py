d600957723553a64695a221ae7e0a4f884101606
Ghiță Bizău <ghita_bizau@yahoo.com>
2020-04-10 16:16:52 +0300

    [refs #116290] Add new script for featured secondary list.

---
+++ src/eni/seis/content/skins/eni_seis/getFeaturedMeetings2.py
@@ -0,0 +1,11 @@
+res = context.portal_catalog.searchResults(
+        portal_type=['eea.meeting'],
+        review_state='published',
+        sort_on='start',
+        sort_order='ascending',
+        path='/east/areas-of-work/communication/events'
+        )
+meetings = [b for b in res]
+featured = [x for x in meetings if x.getObject().is_featured is True]
+
+return featured
