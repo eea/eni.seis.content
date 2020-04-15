498868f302c698e2299d6a51269476b54fe818ed
Ghiță Bizău <ghita_bizau@yahoo.com>
2020-04-15 12:56:17 +0300

    [refs #116290] Add custom behavior for expandable texts.

--- src/eni/seis/content/skins/eni_seis/clc_east.js
+++ src/eni/seis/content/skins/eni_seis/clc_east.js
@@ -1,7 +1,25 @@
-  $(function() {
-    $("#eni-accordion").accordion({
-      active: false,
-      collapsible: true,
-      heightStyle: "content"
-    });
+$(function() {
+  $("#eni-accordion").accordion({
+    active: false,
+    collapsible: true,
+    heightStyle: "content"
   });
+});
+
+$(document).ready(function() {
+  $("tr.more-info").hide();
+
+  $(".btn-more-info.fa-plus").on("click", function() {
+    var data_id = $(this).parent().attr("data-btn-id");
+    $("tr[data-tr-id=" + data_id + "]").show();
+    $(this).removeClass("fa-plus");
+    $(this).addClass("fa-minus");
+  });
+
+  $(".btn-more-info.fa-minus").on("click", function() {
+    var data_id = $(this).parent().attr("data-btn-id");
+    $("tr[data-tr-id=" + data_id + "]").hide();
+    $(this).removeClass("fa-minus");
+    $(this).addClass("fa-plus");
+  });
+});
