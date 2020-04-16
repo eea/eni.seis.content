bfe089c11f0a315e6162638ea8d8f0f7b39f75df
Ghiță Bizău <ghita_bizau@yahoo.com>
2020-04-16 11:02:47 +0300

    [refs #116290] Remove expandable text in events listing - Land section.

--- src/eni/seis/content/skins/eni_seis/clc_east.js
+++ src/eni/seis/content/skins/eni_seis/clc_east.js
@@ -5,21 +5,3 @@ $(function() {
     heightStyle: "content"
   });
 });
-
-$(document).ready(function() {
-  $("tr.more-info").hide();
-
-  $(".btn-more-info.fa-plus").on("click", function() {
-    var data_id = $(this).parent().attr("data-btn-id");
-    $("tr[data-tr-id=" + data_id + "]").show();
-    $(this).removeClass("fa-plus");
-    $(this).addClass("fa-minus");
-  });
-
-  $(".btn-more-info.fa-minus").on("click", function() {
-    var data_id = $(this).parent().attr("data-btn-id");
-    $("tr[data-tr-id=" + data_id + "]").hide();
-    $(this).removeClass("fa-minus");
-    $(this).addClass("fa-plus");
-  });
-});
