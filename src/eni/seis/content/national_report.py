64ff52533cacc08d80540b8475197c9259531d62
Ghiță Bizău <ghita_bizau@yahoo.com>
2020-05-18 18:22:54 +0300

    [refs #107340] Show years in human readable format - national report view.

--- src/eni/seis/content/national_report.py
+++ src/eni/seis/content/national_report.py
@@ -1,6 +1,7 @@
-from zope.interface import implementer
-from plone.dexterity.content import Container
 from eni.seis.content.interfaces import INationalReport
+from itertools import groupby
+from plone.dexterity.content import Container
+from zope.interface import implementer
 
 
 @implementer(INationalReport)
@@ -29,3 +30,67 @@ class NationalReport(Container):
         if self.file:
             return True
         return False
+
+    def grouped_coverage(self, years):
+        """ Given an iterable of numbers, group them by range
+        """
+        def extract_years(data):
+            """
+                2003-2006 -> [2003, 2004, 2005, 2006]
+                2000 -> [2000]
+                abc -> invalid: abc
+            """
+            result = []
+            invalid = False
+            try:
+                year = int(data)
+                result.append(year)
+                return result
+            except ValueError:
+                try:
+                    years = data.split("-")
+                    if len(years) == 2:
+                        start = int(years[0])
+                        end = int(years[1]) + 1
+                        for year in range(start, end):
+                            result.append(year)
+                        return result
+                    else:
+                        invalid = True
+                except Exception:
+                    return "invalid: " + data
+
+            if invalid is True:
+                return "invalid: " + data
+
+        data = []
+        for item in years:
+            temp = extract_years(item)
+            if "invalid" not in temp:
+                for x in temp:
+                    data.append(x)
+
+        # [TODO] In this moment data contains the list of all years. Use this
+        # as search filter later.
+
+        source = [int(x) for x in sorted(set(data))]
+        output = []
+
+        def group_func(idx_nr):
+            """ Used as comparator for grouping
+            """
+            index, number = idx_nr
+            return index - number
+
+        for _key, group in groupby(enumerate(source), group_func):
+            result = [x[1] for x in group]
+
+            if len(result) == 1:
+                output.append("{0}".format(result[0]))
+            else:
+                output.append("{0}-{1}".format(result[0], result[-1]))
+
+        return output
+
+    def human_readable_years(self):
+        return self.grouped_coverage(self.years)
